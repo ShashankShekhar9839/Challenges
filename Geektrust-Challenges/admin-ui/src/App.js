ca23288d8566fe3af574548c7c7db883fdafaec1
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-01-27 14:37:34 +0530

    implemented delete functionality

--- Geektrust-Challenges/admin-ui/src/App.js
+++ Geektrust-Challenges/admin-ui/src/App.js
@@ -1,8 +1,33 @@
-import './App.module.scss';
+import "./App.module.scss";
+import { useEffect, useState } from "react";
+import Table from "./components/Table/Table";
+
 function App() {
-  return (
-     <h1>Admin ui</h1>
-  );
+  const [data, setData] = useState([]);
+  let API =
+    "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+
+  let fetchData = async (API) => {
+    let response = await fetch(API);
+    let jsonData = await response.json();
+    return jsonData; // Make sure to return the data
+  };
+
+  useEffect(() => {
+    fetchData(API)
+      .then((results) => {
+        setData(results);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [API]);
+
+  return (<div>
+         <Table 
+         tableData = {data}
+         />  
+  </div>);
 }
 
 export default App;
---
+++ Geektrust-Challenges/admin-ui/src/components/Table/Table.js
@@ -0,0 +1,54 @@
+import './table.scss';
+import { useState, useEffect } from 'react';
+
+function Table({ tableData }) {
+  const [data, setData] = useState(tableData);
+
+  useEffect(() => {
+    setData(tableData);
+  }, [tableData]);
+
+  const handleDelete = (id) => {
+    setData((prevData) => prevData.filter((item) => item.id !== id));
+  };
+
+  function renderFunction() {
+    return data.map((item, index) => {
+      return (
+        <tr key={index}>
+          <td>
+            <input type="checkbox"></input>
+          </td>
+          <td>{item?.name}</td>
+          <td>{item?.email}</td>
+          <td>{item?.role}</td>
+          <td>
+            <button>Edit</button>
+            <button onClick={() => handleDelete(item.id)}>Delete</button>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  return (
+    <div className='table-wrapper'>
+      <table className='custom-table'>
+        <thead className='table-header'>
+          <tr className='table-header'>
+            <th>
+              <input type='checkbox'></input>
+            </th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>{renderFunction()}</tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
