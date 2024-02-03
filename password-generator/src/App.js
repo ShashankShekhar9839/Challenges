aab21e75625e73c47692ece46d01999ae8d5521e
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-02-03 14:43:01 +0530

    challenge completed

--- password-generator/src/App.js
+++ password-generator/src/App.js
@@ -1,31 +1,69 @@
-import './App.css';
-import {useState} from 'react';
+import "./App.css";
+import { useCallback, useEffect, useRef, useState } from "react";
 
-function App() { 
-     
+function App() {
   const [length, setLength] = useState(8);
-  const [numberAllowed, setNumberAllowed] = useState(false); 
-  const [charAllowed, setCharAllowed] = useState(false); 
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  let passRef = useRef(); 
+
+  let generatePassword = useCallback(() => {
+    let string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let nums = "0123456789";
+    let chars = "@#$%&*-+=/?";
+    if (numberAllowed) string = string + nums;
+    if (charAllowed) string = string + chars;
+    let randomIndex;
+    let pass = "";
+    for (let i = 0; i < length; i++) {
+      randomIndex = Math.floor(Math.random() * string.length);
+      pass = pass + string.charAt(randomIndex);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  useEffect(() => {
+    generatePassword();
+  }, [numberAllowed, charAllowed, length, setPassword]); 
+
+  const handleCopy = () => {
+    passRef.current.select();
+    window.navigator.clipboard.writeText(password);
+  }
 
   return (
-       <div className="outer-wrapper">
-         <h1> Password Generator </h1>
-         <div className = "input-wrapper">
-             <input
-             type = "range"
-             min = {length}
-             max = {30}
-             /> 
-             <label>Password Length</label> 
+    <div className="outer-wrapper">
+      <h1> Password Generator </h1>
+      
+      <div className="top-wrapper">
+      <input type="string" value={password} readOnly  ref={passRef}/>
+      <button onClick={handleCopy}>Copy Password</button>
+      </div>
 
-             <input type = "checkbox"/>
-             <label>Character Allowed</label> 
+      <div className="input-wrapper">
+        <input
+          type="range"
+          min={8}
+          max={30}
+          value={length}
+          onChange={(e) => {
+            setLength(e.target.value);
+          }}
+        />
+        <label>Password Length : {length}</label>
 
-             <input type = "checkbox"/>
-             <label>Number Allowed</label>
+        <input type="checkbox" onChange={() => setCharAllowed(!charAllowed)} />
+        <label>Character Allowed</label>
 
-         </div>
-       </div>
+        <input
+          type="checkbox"
+          onChange={() => setNumberAllowed(!numberAllowed)}
+        />
+        <label>Number Allowed</label>
+      </div>
+    </div>
   );
 }
 
